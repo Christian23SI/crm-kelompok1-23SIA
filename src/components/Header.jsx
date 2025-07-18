08f3c8b60bfc019eb1ebcc8ba41a69ccc88c4b2c
ChristianCarlos <christian23si@mahasiswa.pcr.ac.id>
2025-07-18 16:15:40 +0700

    perubahan sedikit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,6 +14,7 @@ const Header = () => {
       '/faq': 'FAQ',
       '/inventaris': 'Inventory Management',
       '/feedback': 'Feedback Management',
+      '/predict': 'Prediction Page',
       '/email': 'Email Notification',
       '/akun': 'Account Settings',
       '/user': 'User Management'
